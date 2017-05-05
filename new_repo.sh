f0546c84e97c71bca5585bd50bb5ecaf26c1cb7d
Kayoo <jeri.dsql@gmail.com>
2017-05-05 11:29:17 +0200

    mise a jour avec le nouveau site

--- new_repo.sh
+++ new_repo.sh
@@ -3,22 +3,22 @@
 # Script pour récupération de la liste des dépôts deepin
 ##############
 url="/var/www/xxxx/index.html"
-file_tmp="/tmp/mirror.html"
+file_tmp="/tmp/index.html"
 
 ## extraction/récupétation de la liste
 rm -f $file_tmp
-wget -P /tmp https://www.deepin.org/mirror.html
+wget -P /tmp https://www.deepin.org/en/mirrors/packages/
 if [ ! $? -eq 0 ]; then
   echo "Impossible de récupérer le mirror.html officiel"
   #> Envoyer un mail
   exit 1
 fi
-grep "  <a href=\"http" $file_tmp | awk -F= '{ print $2}' | awk -F\> '{ print $1}' > /tmp/mirror_formated.html
+grep "<td ><a href=\"http" $file_tmp | awk -F= '{ print $2 }' | awk '{ print $1 }' > /tmp/mirror_formated.html
 mv -f /tmp/mirror_formated.html $file_tmp
 
 ## Supression de certaines lignes
-sed -i '/system.html/d' $file_tmp
-sed -i '/bbs.deepin.org/d' $file_tmp
+#sed -i '/system.html/d' $file_tmp
+#sed -i '/bbs.deepin.org/d' $file_tmp
 
 ## Test du contenu du fichier
 test -s $file_tmp
